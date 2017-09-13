6a071316dd41db2ce323190774de6cf4088f1938
iq-mschmeets <mark@inquisient.com>
2017-09-13 15:45:20 -0400

    Fix greater than escaping in data.

--- src/common/ReadOnlyInputFormGroup.js
+++ src/common/ReadOnlyInputFormGroup.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 // No need to manage state, because read only.
 class ReadOnlyInputFormGroup extends React.Component{
+    _getValue(){
+        if( !this.props.value ){
+            return '';
+        }
+        if(Number.isInteger(this.props.value)){
+            return this.props.value;
+        }
+        return this.props.value.length > 0 ? this.props.value.toString().replace('&gt;', '>') :'';
+    }
     render(){
+        console.log("ROIFG: value = %s",this.props.value);
+
         return (<div className="form-group row">
                 <label className="col-xs-2 control-label" htmlFor="relationship_name">{this.props.label}:</label>
                 <div className="col-xs-10">
                     <input type="text"
                            placeholder="" className="form-control input-md"
-                           readOnly="true" value={this.props.value} />
+                           readOnly="true" value={this._getValue()} />
                     <em className="help-block">{this.props.help}</em>
                 </div>
             </div>);
